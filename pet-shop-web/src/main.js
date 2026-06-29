5ae0626b4f2eeebe09ee618bed785de7d6f97df0
linhy1122 <1768013886@qq.com>
2026-06-29 15:43:17 +0800

    fix

--- pet-shop-web/src/main.js
+++ pet-shop-web/src/main.js
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import '@/styles/theme.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
